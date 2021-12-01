c643c17dabc5b465132921ab8bf53701d3d16cca
NgocMinhThuNguyen <ngocminhthu.nguyen@my.jcu.edu.au>
2021-12-01 12:48:44 +0700

    Update lists exercise

---
+++ list_exercises.py
@@ -0,0 +1,23 @@
+# 1. Basic list operations
+numbers = []
+
+for i in range(5):
+    number = float(input("Number: "))
+    numbers.append(number)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of numbers is {sum(numbers) / len(numbers)}")
+
+# 2. Woefully inadequate security checker
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+user_name = input("Your username: ")
+if user_name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
